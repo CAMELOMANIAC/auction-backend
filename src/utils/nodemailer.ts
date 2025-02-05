272793af9567a4511411944e663cc62feccfa070
yugyusang <yugyusang12@gmail.com>
2025-02-05 23:55:20 +0900

    feat: 메일 전송함수 보강

--- src/utils/nodemailer.ts
+++ src/utils/nodemailer.ts
@@ -8,17 +8,30 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-const mailOptions = {
-  from: "your-email@gmail.com",
-  to: "user-email@example.com",
-  subject: "인증 메일",
-  text: "인증 링크입니다",
-};
+export const sendMail = ({
+  to,
+  subject,
+  text,
+  html,
+}: {
+  to: string;
+  subject: string;
+  text?: string;
+  html?: string;
+}) => {
+  const mailOptions = {
+    from: "your-email@gmail.com",
+    to: to,
+    subject: subject,
+    text: text,
+    html: html,
+  };
 
-transporter.sendMail(mailOptions, (error) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("메일 보내기 성공");
-  }
-});
+  transporter.sendMail(mailOptions, (error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("메일 보내기 성공");
+    }
+  });
+};
