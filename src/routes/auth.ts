cf870871567f98978fd60b03770766948251f23b
yugyusang <yugyusang12@gmail.com>
2025-02-06 17:47:16 +0900

    feat: 정의한 이메일인증,로그인 계층을 auth 라우트에 연결

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,11 @@
 import { Router } from "express";
-import registerUser from "../services/registerUser.service";
+import registerUser, { verifyEmail } from "../services/registerUser.service";
+import { loginUser } from "../services/LoginUser.service";
+
 const router = Router();
+
 router.get("/auth/register", registerUser);
+router.get("/auth/verify-email", verifyEmail);
+router.get("/auth/login", loginUser);
 
 export default router;
