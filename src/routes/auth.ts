37bfcefb9c99352969efd03adff657ef952bf7f4
yugyusang <yugyusang12@gmail.com>
2025-02-07 23:14:46 +0900

    feat: 중복체크 api라우트 정의

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,24 @@
 import { Router } from "express";
-import registerUser, { verifyEmail } from "../services/registerUser.service";
-import { loginUser } from "../services/LoginUser.service";
+import registerUser, {
+  checkRegisterEmail,
+  checkRegisterId,
+  checkRegisterNickname,
+  verifyEmail,
+} from "../services/registerUser.service";
+import { loginUser } from "../services/loginUser.service";
 
 const router = Router();
 
-router.get("/auth/register", registerUser);
-router.get("/auth/verify-email", verifyEmail);
-router.get("/auth/login", loginUser);
+//회원가입
+router.post("/auth/register", registerUser);
+router.post("/auth/verify-email", verifyEmail);
+
+//중복체크
+router.get("/auth/id-duplication", checkRegisterId);
+router.get("/auth/nickname-duplication", checkRegisterEmail);
+router.get("/auth/email-duplication", checkRegisterNickname);
+
+//로그인
+router.post("/auth/login", loginUser);
 
 export default router;
