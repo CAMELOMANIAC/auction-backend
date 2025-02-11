b8b8deca6ce7dbc53cc723a4999d90b874f4a905
yugyusang <yugyusang12@gmail.com>
2025-02-11 12:39:32 +0900

    feat: 클라이언트에서 인증헤더를 사용할수있도록 cors정책 추가

--- src/middlewares/corsMiddleware.ts
+++ src/middlewares/corsMiddleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 
 const corsMiddleware = (req: Request, res: Response, next: NextFunction): void => {
   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Max-Age", "86400");
