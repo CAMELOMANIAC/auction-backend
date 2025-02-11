8264003a6658cc1f1896d82739496a7fe22bb746
yugyusang <yugyusang12@gmail.com>
2025-02-11 12:41:00 +0900

    fix: 유저 불일치 에러 메세지 수정

--- src/utils/errorCode.ts
+++ src/utils/errorCode.ts
@@ -22,7 +22,7 @@ export default ErrorCode;
 export const errorCodeAnswer: { [key: number]: { status: number; message: string } } = {
   [ErrorCode.NO_ROWS_AFFECTED]: { status: 404, message: "조건에 맞지 않아 행을 변경하지 못했습니다" },
   [ErrorCode.EMAIL_VERIFICATION_NOT_FOUND]: { status: 404, message: "이메일 인증 토큰이 없습니다" },
-  [ErrorCode.USER_NOT_FOUND]: { status: 404, message: "사용자가 없습니다" },
+  [ErrorCode.USER_NOT_FOUND]: { status: 404, message: "일치하는 사용자가 없습니다" },
   [ErrorCode.ACCESS_TOKEN_EXPIRED]: { status: 401, message: "토큰이 만료되었습니다 재발급 요청을 시작해주세요" },
   [ErrorCode.ID_DUPLICATED]: { status: 400, message: "이 아이디는 다른 사용자가 이용중입니다" },
   [ErrorCode.EMAIL_DUPLICATED]: { status: 400, message: "이 메일은 다른 사용자가 이용중입니다" },
