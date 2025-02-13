bfbad08aab7f632dd654f5da9c63743043e421bd
yugyusang <yugyusang12@gmail.com>
2025-02-13 20:43:23 +0900

    feat: 로그아웃 라우트 적용

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,16 +1,19 @@
 import { Router } from "express";
-import registerUser, { verifyEmail } from "../services/registerUser.service";
+import registerAccount, { verifyEmail } from "../services/registerUser.service";
 import { loginUser } from "../services/loginUser.service";
-import { refleshAccessToken as refreshAccessToken } from "../services/auth.service";
+import { logout, refleshAccessToken as refreshAccessToken } from "../services/auth.service";
+import authMiddleware from "../middlewares/authMiddleware";
 
 const router = Router();
 
 //회원가입
-router.post("/register", registerUser);
+router.post("/register", registerAccount);
 router.delete("/verify-email/:code", verifyEmail);
 
 //로그인
 router.post("/login", loginUser);
+//로그아웃
+router.delete("/logout", authMiddleware, logout);
 
 //액세스토큰 재발급
 router.post("/refresh-token", refreshAccessToken);
