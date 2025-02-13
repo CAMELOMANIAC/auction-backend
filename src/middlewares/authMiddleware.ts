d5c0f9034bf699232874bc506268ddbbb0645c13
yugyusang <yugyusang12@gmail.com>
2025-02-13 15:21:37 +0900

    feat: 다른 통신함수처럼 에러핸들링 하도록 변경 및 액세스토큰 요청방식을 쿠키에서 인증헤더로 수정

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import ErrorCode from "../utils/errorCode";
+import ErrorCode, { errorCodeAnswer } from "../utils/errorCode";
+import { handlerError } from "../utils/fuction";
 
 export type UserAuthRequest = Request & {
   user?: string;
@@ -14,33 +15,26 @@ export type UserAuthRequest = Request & {
  * @param {NextFunction} next - 다음 미들웨어로 이동하는 함수
  */
 const authMiddleware = (req: UserAuthRequest, res: Response, next: NextFunction) => {
-  const token = req.cookies.accessToken;
-
-  if (!token) {
-    res.status(401).json({ message: "인증 토큰이 없습니다." });
-    return;
-  }
-
+  const authHeader = req.headers.authorization;
+  const token = authHeader?.split(" ")[1];
   try {
+    if (!token || token.split(".").length !== 3) {
+      throw new Error(errorCodeAnswer[ErrorCode.ACCESS_TOKEN_REQUIRED].message);
+    }
     if (!process.env.JWT_ACCESS_SECRET_KEY) {
-      res.status(500).json({ message: "서버 JWT 키를 찾을 수 없습니다" });
-      return;
+      throw new Error(errorCodeAnswer[ErrorCode.INVALID_ENVIRONMENT_VARIABLE].message);
     }
     const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET_KEY);
     if (typeof decoded === "string" || !decoded.sub) {
-      throw new Error("JWT 키를 찾을 수 없습니다");
+      throw new Error(errorCodeAnswer[ErrorCode.INVAILD_ACCESS_TOKEN].message);
     }
     if (decoded.exp && decoded.exp < Date.now() / 1000) {
-      console.log("토큰이 만료되었습니다 재발급 요청을 시작하세요");
-      res
-        .status(401)
-        .json({ error: ErrorCode.ACCESS_TOKEN_EXPIRED, message: "토큰이 만료되었습니다 재발급 요청을 시작하세요." });
-      return;
+      throw new Error(errorCodeAnswer[ErrorCode.ACCESS_TOKEN_EXPIRED].message);
     }
     req.user = decoded.sub;
     next();
   } catch (error) {
-    res.status(401).json({ message: "유효하지 않은 토큰입니다." });
+    handlerError(error, res);
   }
 };
 
