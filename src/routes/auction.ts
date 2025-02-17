7b3761b3cc7a70b6f36dc98aabc9cda82b9030c1
yugyusang <yugyusang12@gmail.com>
2025-02-17 21:13:05 +0900

    feat: /auction 라우트에 구현한 서비스함수 getAuctionDetail, getAuctionImage, getBidList,regiserBid, getViewerCount등록

--- src/routes/auction.ts
+++ src/routes/auction.ts
@@ -1,6 +1,14 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/authMiddleware";
-import { registerAuction } from "../services/auction.service";
+import {
+  getAuctionDetail,
+  getAuctionImage,
+  getAuctionList,
+  getBidList,
+  getViewerCount,
+  regiserBid,
+  registerAuction,
+} from "../services/auction.service";
 import upload from "../middlewares/binaryParserMiddleware";
 const router = Router();
 
@@ -9,5 +17,12 @@ const binaryParser = upload.fields([
   { name: "subImage", maxCount: 3 },
 ]);
 router.post("/", authMiddleware, binaryParser, registerAuction);
+router.get("/", getAuctionList);
+
+router.get("/:auctionId/detail", getAuctionDetail);
+router.get("/:auctionId/image", getAuctionImage);
+router.get("/:auctionId/bid", getBidList);
+router.post("/:auctionId/bid", authMiddleware, regiserBid);
+router.get("/:auctionId/viewer-count", getViewerCount);
 
 export default router;
